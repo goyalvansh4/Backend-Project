b82e359373a8541d773afaf158c8dc5158f2b1fb
goyalvansh4 <vanshgupta7668@gmail.com>
2024-05-06 23:17:23 +0530

    write wrapper fnc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import dotenv from "dotenv";
 import connectDb from "./db/index.js";
-
+import app from "./app.js"
 dotenv.config({
   path:'./env'
 });
 
-connectDb();
+connectDb()
+.then(()=>{
+  app.listen(process.env.PORT || 8000 , ()=>{
+    console.log("Server is started at:",process.env.PORT);
+  })
+})
+.catch((err)=>{
+  console.log(`Database Connection Fail`,err);
+})
 
