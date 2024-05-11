5ca600f846fd0de30ac263e6c40c80c10b398249
goyalvansh4 <vanshgupta7668@gmail.com>
2024-05-11 12:07:06 +0530

    setup some simple route for testing

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
+const app =express();
+
 app.use(cors({
   origin:process.env.CORS_ORIGIN,
   credentials:true
@@ -12,7 +14,13 @@ app.use(express.urlencoded({extended:true,limit:true}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes import
 
-const app =express();
+import userRouter from './routes/user.routes.js';
+
+app.use("/api/v1/users",userRouter);
+
+
+
 
 export {app};
